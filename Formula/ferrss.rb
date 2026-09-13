dba03a44c685ad1c7b6662fd203382fef77f4859
oouxx <wangxinxing961129@gmail.com>
2026-09-13 21:58:49 +0800

    ferrss 0.3.14

--- Formula/ferrss.rb
+++ Formula/ferrss.rb
@@ -1,17 +1,17 @@
 class Ferrss < Formula
   desc "AI-driven CLI tool that turns websites into command-line interfaces"
   homepage "https://github.com/oouxx/Ferrss"
-  version "0.3.13"
+  version "0.3.14"
   license "Apache-2.0"
 
   on_arm do
-    url "https://github.com/oouxx/Ferrss/releases/download/v0.3.13/ferrss-aarch64-apple-darwin.tar.gz"
-    sha256 "c8b6ead70b6ba18cdaaf8f3752a4e53991d7636bfef184fc104c66925c761017"
+    url "https://github.com/oouxx/Ferrss/releases/download/v0.3.14/ferrss-aarch64-apple-darwin.tar.gz"
+    sha256 "840d72e1b33eba48ba33d8a95f78ff64104544b1c4b2218daa1c18c2f17eb535"
   end
 
   on_intel do
-    url "https://github.com/oouxx/Ferrss/releases/download/v0.3.13/ferrss-x86_64-apple-darwin.tar.gz"
-    sha256 "d49790d9f594b2944f1c6afbee484c6abdc10ef0f5e61d9c4c439811a2332b3b"
+    url "https://github.com/oouxx/Ferrss/releases/download/v0.3.14/ferrss-x86_64-apple-darwin.tar.gz"
+    sha256 "79d75aeb51d076e3c0e9fbc181d4050b76dfd782dd2f3a82bafe0fd6fed2997d"
   end
 
   def install
